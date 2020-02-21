959f2df0186bed0267f08ce010b004a5ce3f0bc6
Devin <alexanderdev117@gmail.com>
2020-02-21 14:20:09 -0600

    changes to model, fixes for server routes.

--- Develop/seeders/seed.js
+++ Develop/seeders/seed.js
@@ -8,12 +8,12 @@ mongoose.connect("mongodb://localhost/workout", {
 
 let workoutSeed = [
   {
-    day: new Date().setDate(new Date().getDate()-10),
+    day: new Date().setDate(new Date().getDate() - 10),
     exercises: [
       {
         type: "resistance",
         name: "Bicep Curl",
-        duration: 20,
+        totalDuration: 20,
         weight: 100,
         reps: 10,
         sets: 4
@@ -21,12 +21,12 @@ let workoutSeed = [
     ]
   },
   {
-    day: new Date().setDate(new Date().getDate()-9),
+    day: new Date().setDate(new Date().getDate() - 9),
     exercises: [
       {
         type: "resistance",
         name: "Lateral Pull",
-        duration: 20,
+        totalDuration: 20,
         weight: 300,
         reps: 10,
         sets: 4
@@ -34,12 +34,12 @@ let workoutSeed = [
     ]
   },
   {
-    day: new Date().setDate(new Date().getDate()-8),
+    day: new Date().setDate(new Date().getDate() - 8),
     exercises: [
       {
         type: "resistance",
         name: "Push Press",
-        duration: 25,
+        totalDuration: 25,
         weight: 185,
         reps: 8,
         sets: 4
@@ -47,23 +47,23 @@ let workoutSeed = [
     ]
   },
   {
-    day: new Date().setDate(new Date().getDate()-7),
+    day: new Date().setDate(new Date().getDate() - 7),
     exercises: [
       {
         type: "cardio",
         name: "Running",
-        duration: 25,
+        totalDuration: 25,
         distance: 4
       }
     ]
   },
   {
-    day: new Date().setDate(new Date().getDate()-6),
+    day: new Date().setDate(new Date().getDate() - 6),
     exercises: [
       {
         type: "resistance",
         name: "Bench Press",
-        duration: 20,
+        totalDuration: 20,
         weight: 285,
         reps: 10,
         sets: 4
@@ -71,12 +71,12 @@ let workoutSeed = [
     ]
   },
   {
-    day: new Date().setDate(new Date().getDate()-5),
+    day: new Date().setDate(new Date().getDate() - 5),
     exercises: [
       {
         type: "resistance",
         name: "Bench Press",
-        duration: 20,
+        totalDuration: 20,
         weight: 300,
         reps: 10,
         sets: 4
@@ -84,12 +84,12 @@ let workoutSeed = [
     ]
   },
   {
-    day: new Date().setDate(new Date().getDate()-4),
+    day: new Date().setDate(new Date().getDate() - 4),
     exercises: [
       {
         type: "resistance",
         name: "Quad Press",
-        duration: 30,
+        totalDuration: 30,
         weight: 300,
         reps: 10,
         sets: 4
@@ -97,12 +97,12 @@ let workoutSeed = [
     ]
   },
   {
-    day: new Date().setDate(new Date().getDate()-3),
+    day: new Date().setDate(new Date().getDate() - 3),
     exercises: [
       {
         type: "resistance",
         name: "Bench Press",
-        duration: 20,
+        totalDuration: 20,
         weight: 300,
         reps: 10,
         sets: 4
@@ -110,12 +110,12 @@ let workoutSeed = [
     ]
   },
   {
-    day: new Date().setDate(new Date().getDate()-2),
+    day: new Date().setDate(new Date().getDate() - 2),
     exercises: [
       {
         type: "resistance",
         name: "Military Press",
-        duration: 20,
+        totalDuration: 20,
         weight: 300,
         reps: 10,
         sets: 4
@@ -123,20 +123,21 @@ let workoutSeed = [
     ]
   },
   {
-    day: new Date().setDate(new Date().getDate()-1),
+    day: new Date().setDate(new Date().getDate() - 1),
     exercises: [
       {
         type: "resistance",
         name: "Bench",
-        duration: 30,
+        totalDuration: 30,
         distance: 2
       }
     ]
   }
 ];
 
-db.Workout.deleteMany({})
-  .then(() => db.Workout.collection.insertMany(workoutSeed))
+db.workout
+  .deleteMany({})
+  .then(() => db.workout.collection.insertMany(workoutSeed))
   .then(data => {
     console.log(data.result.n + " records inserted!");
     process.exit(0);
--- Develop/server.js
+++ Develop/server.js
@@ -1,44 +1,42 @@
 const express = require("express");
 const logger = require("morgan");
+const PORT = process.env.PORT || 3000;
 const mongoose = require("mongoose");
+const Workout = require("./models/workout");
+const app = express();
 const path = require("path");
 
-const PORT = process.env.PORT || 3000;
-
-const db = require("./models/workout");
-
-const app = express();
-
 app.use(logger("dev"));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "./public/index.html"));
+  res.status(200).sendFile(path.join(__dirname + "./public/index.html"));
 });
 
 app.get("/exercise", (req, res) => {
-  res.sendFile(path.join(__dirname + "/public/exercise.html"));
+  res.status(200).sendFile(path.join(__dirname + "/public/exercise.html"));
 });
 
 app.get("/stats", (req, res) => {
-  res.sendFile(path.join(__dirname + "/public/stats.html"));
+  res.status(200).sendFile(path.join(__dirname + "/public/stats.html"));
 });
 
-app.get("/all", (req, res) => {
-  db.workouts.find({}, (error, data) => {
-    if (error) {
-      res.send(error);
-    } else {
-      res.json(data);
-    }
-  });
-})
+app.get("/api/workouts", (req, res) => {
+  db.Workout.find({})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
